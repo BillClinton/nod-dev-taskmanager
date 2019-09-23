a004aac5bf67727195eb34574e972c73aaedf15c
BillClinton <bclinton@appunto.net>
2019-09-23 12:40:29 -0400

    Add avatar to user model

--- src/models/user.js
+++ src/models/user.js
@@ -51,7 +51,10 @@ const userSchema = new mongoose.Schema(
           required: true
         }
       }
-    ]
+    ],
+    avatar: {
+      type: Buffer
+    }
   },
   {
     timestamps: true
@@ -83,6 +86,7 @@ userSchema.methods.toJSON = function() {
 
   delete userObject.password;
   delete userObject.tokens;
+  delete userObject.avatar;
 
   return userObject;
 };
