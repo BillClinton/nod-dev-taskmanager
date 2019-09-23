40802da28e151a53b650fc7694e82b216daa0491
BillClinton <bclinton@appunto.net>
2019-09-23 12:38:54 -0400

    Remove test code

--- src/index.js
+++ src/index.js
@@ -2,19 +2,11 @@ require('dotenv').config();
 
 const express = require('express');
 require('./db/mongoose.js');
-
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-// Register middleware function
-app.use((req, res, next) => {
-  console.log(req.method, req.path);
-
-  next();
-});
 
 app.use(express.json());
 app.use(userRouter);
